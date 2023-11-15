7b3e00eae25d89ef6e3f1ba3086407030bf71ca8
hm904049@gmail.com <you@example.com>
2023-11-15 14:21:25 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_Dot = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+        
+        if HYPEN_E_Dot in requirements:
+            requirements.remove(HYPEN_E_Dot)
+    
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Hammad',
+    author_email='hm904049@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
